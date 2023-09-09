6d51e1d337408a221659b995969bd2c31e93f8fe
David Nguyen <nguy3482@umn.edu>
2023-09-09 15:34:47 -0500

    add disclamer and info button

--- src/pages/Bullet.tsx
+++ src/pages/Bullet.tsx
@@ -1,3 +1,5 @@
+import { faInfoCircle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
 import CountdownApp from "src/components/timerDate";
 
@@ -7,9 +9,6 @@ export default function BulletTime() {
   );
 
   const swapPlaylist = (e: React.ChangeEvent<HTMLInputElement>) => {
-    // Make this more generalizable, split into new route
-    // https://developers.google.com/youtube/v3/docs/playlistItems/list
-    // Use youtube api to cue first vid
     if (e.target.checked) {
       // Star
       setPlaylist("OLAK5uy_n64BgoFQqplG5_U72otFKMswV0vaKEjOM");
@@ -24,18 +23,42 @@ export default function BulletTime() {
 
   return (
     <div>
-      <div className="text-5xl font-ironman text-center bg-stone-800 text-white p-2">
+      <div className="text-5xl font-ironman sm:text-left text-center bg-base-400 text-white p-2">
         <label className="swap">
           <input type="checkbox" onChange={(e) => swapPlaylist(e)} />
           <span className="swap-off">Bullet❤️ Timer</span>
           <span className="swap-on">Bullet⭐ Timer</span>
         </label>
+        <div
+          className="tooltip tooltip-left 
+          text-sm font-sans absolute right-2 top-2"
+          data-tip="Click the emoji to swap games!"
+        >
+          <FontAwesomeIcon icon={faInfoCircle} />
+        </div>
       </div>
       <CountdownApp
         videoId={videoId}
         playlist={playlist}
         initialTimeLimit={180}
       />
+      <p className="text-sm font-sans italic text-white p-1">
+        This website is not associated with{" "}
+        <a
+          href="https://www.level99store.com"
+          className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600"
+        >
+          Level 99 Games
+        </a>{" "}
+        or the{" "}
+        <a
+          href="https://www.level99store.com/products/bullet-star"
+          className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600"
+        >
+          Bullet franchise.
+        </a>
+        .
+      </p>
     </div>
   );
 }
